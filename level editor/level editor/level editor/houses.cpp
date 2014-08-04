9392df38f9a22a28a036e0a7d17e8e7c24085a8e
Anton Johansson <plantpaladin@gmail.com>
2014-08-04 16:44:38 +0200

    Load is progressing
    The load function is working in the drawer file but not in the house
    function

--- level editor/level editor/level editor/houses.cpp
+++ level editor/level editor/level editor/houses.cpp
@@ -2,6 +2,7 @@
 #include "singleton.h"
 #include <SFML\Graphics.hpp>
 #include <sstream>
+#include <iostream>
 Singleton *singleton_p=Singleton::newSingleton();
 using namespace std;
 
@@ -57,25 +58,38 @@ d_maxY=d_size*34+y;
 ;
 
 houses::houses(string loaddata){
-	std::stringstream ss;
-	int x=1; int y=2;//placeholder
-	d_size=singleton_p->getSize();
-	d_type=singleton_p->getDirection();
-	d_x=x;d_y=y;
+
+	stringstream ss(loaddata);
+	string line;
+	
+	getline (ss,line,'¤');//converting the blocks seperated by "¤" into the relevant variables
+	istringstream is(line);
+	is>>d_size;
+
+	getline (ss,line,'¤');
+	istringstream is1(line);
+	is1>>d_x;
+
+	getline (ss,line,'¤');
+	istringstream is2(line);
+	is2>>d_y;
+	cout<<line;
+	getline (ss,line,'¤');
+	istringstream is3(line);
+	is3>>d_type;
 	rectangles.resize(d_size);
 	
 	if (d_type==0){
 		for(int i=0;i<d_size;i++)
-		{
-			
+		{			
 rectangles[i]=new sf::RectangleShape;
-rectangles[i]->setPosition(x+i*32,y);
+rectangles[i]->setPosition(d_x+i*32,d_y);
 rectangles[i]->setSize(sf::Vector2f(28, 28));
 rectangles[i]->setFillColor(dumbdata());
 rectangles[i]->setOutlineColor(sf::Color::Cyan);
 rectangles[i]->setOutlineThickness(2);
-d_maX=d_size*34+x;
-d_maxY=y+34;
+d_maX=d_size*34+d_x;
+d_maxY=d_y+34;
 		}
 	}
 	else if (d_type==1)
@@ -84,13 +98,13 @@ d_maxY=y+34;
 		{
 			
 rectangles[i]=new sf::RectangleShape;
-rectangles[i]->setPosition(x,y+i*32);
+rectangles[i]->setPosition(d_x,d_y+i*32);
 rectangles[i]->setSize(sf::Vector2f(28, 28));
 rectangles[i]->setFillColor(dumbdata());
 rectangles[i]->setOutlineColor(sf::Color::White);
 rectangles[i]->setOutlineThickness(2);
-d_maX=x+34;
-d_maxY=d_size*34+y;
+d_maX=d_x+34;
+d_maxY=d_size*34+d_y;
 	
 	};
 
@@ -112,11 +126,11 @@ const int houses::getMaxY(){return d_maxY;}
 const string houses::getDataAsString()
 {
 	std::stringstream ss;
-	ss << d_size<<",";
-	ss << d_x<<",";
-	ss << d_y<<",";
-	ss << d_type<<",";
-	ss << d_maX<<",";
+	ss << d_size<<"¤";
+	ss << d_x<<"¤";
+	ss << d_y<<"¤";
+	ss << d_type<<"¤";
+	ss << d_maX<<"¤";//the last two variables are not loaded but is useful for a human reader
 	ss << d_maxY;
 	return ss.str();
 }
